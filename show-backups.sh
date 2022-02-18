3c4bcf45c8dd30f8bcf32bdf84b8fd7006484bde
Robert Smith <rsmith@rsmith-mac.myfiosgateway.com>
2022-02-18 14:05:45 -0500

    backups

---
+++ show-backups.sh
@@ -0,0 +1,5 @@
+sudo timeshift --list --snapshot-device /dev/sdb1 >> ~/Desktop/backups.txt
+echo "blahblahblah"
+echo ""
+sudo timeshift --list --snapshot-device /dev/sda1 >> ~/Desktop/backups.txt
+echo ""
