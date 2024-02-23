9503bd79d88b1b577e6aff2ed9ee872471488cf5
RobSmith <rcs@rcs.fractal.mint>
2024-02-22 20:21:36 -0500

    more ssh

--- NotReady/SSH.Notes.sh
+++ NotReady/SSH.Notes.sh
@@ -12,3 +12,4 @@ ip a
 ## This is the best one
 hostname -I
 # 192.168.1.172 172.18.0.1 172.17.0.1 fdad:a352:f722:e11c:18b6:a11e:3a31:801c fdad:a352:f722:e11c:d4b5:da0f:e5a0:76c0 
+19:        inet 192.168.1.172  netmask 255.255.255.0  broadcast 192.168.1.255
